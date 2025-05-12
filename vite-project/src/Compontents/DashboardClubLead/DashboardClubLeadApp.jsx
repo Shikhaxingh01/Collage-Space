8708bcaf3a646c265b1b263111efcfb6181ab1dd
Shikha Singh <121444818+Shikhaxingh01@users.noreply.github.com>
2025-05-13 02:12:29 +0530

    Delete vite-project directory

--- vite-project/src/Compontents/DashboardClubLead/DashboardClubLeadApp.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import Dashboard from './pages/Dashboard'
-export function DashboardClubLeadApp() {
-  return <Dashboard />
-}
--- vite-project/src/Pages/DashBoard/ClubLeadDashBoard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ClubLeadDashBoard = () => {
-  return (
-    <div>ClubLeadDashBoard</div>
-  )
-}
-
-export default ClubLeadDashBoard
